7b0ec7f65bee485793315f461edfddb588979221
1AlexDev <157933564+1AlexDev@users.noreply.github.com>
2024-06-07 21:20:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, request, send_file, render_template_string, after_this_request, redirect
-from pytube import YouTube
-import tempfile
-import os
+from flask import Flask, request, render_template_string, redirect, jsonify
+import requests
 
 app = Flask(__name__)
+
+backend_url = 'https://icv-backend.onrender.com/download'
 
 @app.route('/')
 def index():
@@ -20,37 +20,27 @@ def index():
             <div class="container">
                 <h1>YT -> MP4 🎥</h1>
                 <p>With this Youtube -> MP4 converter, <br>you can download any YouTube video<br> for completely <b>FREE</b>!</p>
-                <form action="/download" method="post">
-                    <input type="text" name="url" placeholder="Enter YouTube URL" required>
+                <form id="download-form">
+                    <input type="text" id="url" name="url" placeholder="Enter YouTube URL" required>
                     <br><br>
                     <button type="submit" class="submit" id="submit">Download</button>
                 </form>
             </div>
-            <script src="static/main.js"></script>
+            <script src="/static/main.js"></script>
         </body>
         </html>
     ''')
 
-# @app.route('/download', methods=['POST'])
-# def download():
-#     url = request.form['url']
-#     yt = YouTube(url)
-#     stream = yt.streams.filter(progressive=True, file_extension='mp4').first()
-#     
-#     # Create a temporary file
-#     temp_dir = tempfile.gettempdir()
-#     temp_file_path = os.path.join(temp_dir, f"{yt.title}.mp4")
-#     stream.download(output_path=temp_dir, filename=f"{yt.title}.mp4")
-#     
-#     @after_this_request
-#     def remove_file(response):
-#         try:
-#             os.remove(temp_file_path)
-#         except Exception as error:
-#             app.logger.error("Error removing downloaded file: %s", error)
-#         return response
-# 
-#     return send_file(temp_file_path, as_attachment=True, download_name=f"{yt.title}.mp4")
+@app.route('/download', methods=['POST'])
+def download():
+    data = request.json
+    url = data['url']
+    response = requests.post(backend_url, json={'url': url})
+    
+    if response.status_code == 200:
+        return jsonify({'message': 'Download started successfully'}), 200
+    else:
+        return jsonify({'error': 'Failed to start download'}), response.status_code
 
 if __name__ == '__main__':
     app.run(debug=True)
